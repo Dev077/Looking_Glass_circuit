a06cec4f6e8ea3844fcf131e47a5f4b36fb472eb
Dev <dchaud26@uwo.ca>
2026-09-03 00:02:45 -0400

    this is irritating

--- cache_activations.py
+++ cache_activations.py
@@ -18,7 +18,7 @@ import os
 import torch
 from tqdm import tqdm
 from transformer_lens import HookedTransformer, utils
-from lgataset import LOOKING_GLASS_DATASET
+from lgdataset import LOOKING_GLASS_DATASET
 
 
 # ---------------------------------------------------------------------------
